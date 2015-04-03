18c9da63547d80a78838a82725667e06371631de
Ian Kilgore <imkilgor@ncsu.edu>
2015-04-02 23:25:33 -0400

    Damped driven pendulum, damped/periodic/chaotic behaviour & poincare section

--- damped_driven_ode.m
+++ damped_driven_ode.m
@@ -1,30 +1,21 @@
-% theta'' = -g/l sin(theta) - q theta' + Fd*sin(omega_d*t)
-
-% http://www.thphys.uni-heidelberg.de/~gasenzer/index.php?n1=teaching&n2=chaos
-
-% Rewrite in dimensionless form 
-% theta''
-
-% rewrite as system of 1st order ODEs where
-
-% x1dot(t) = x2(t)
-% x2dot(t) = -g/l*sin(x1(t)) - q*x2(t) + F_d*sin(omega_d, t)
-
-% Now note x1 = theta, x2 = theta', x2dot = theta''
-
-% F_d amplitude of periodic driving force
-% theta_d frequency of same
-% q is damping constant
+%% TODO: write out math here. for now marked page in my notebook
 
 function xdot = damped_driven_ode( t, x, param)
-l = param(1);
-g = param(2);
-q = param(3);
-omega_d = param(4);
-F_d = param(5);
+    % wdot, thetadot, phidot as functions of w, theta, phi
+    % params q, g, omega_d
 
-    x1dot = x(2);
-    x2dot = -g/l*x(1) - q*x(2) + F_d*sin(omega_d*t);
-    xdot = [x1dot; x2dot];
+    q = param(1);
+    g = param(2);
+    omega_d = param(3);
+
+    omega = x(1);
+    theta = x(2);
+    phi = x(3);
+    
+    omegadot = -1/q*omega - sin(theta) + g*cos(phi);
+    thetadot = omega;
+    phidot = omega_d;
+    
+    xdot = [omegadot; thetadot; phidot];
 end
 
--- damped_driven_pendulum.m
+++ damped_driven_pendulum.m
@@ -1,15 +1,59 @@
+clear all
+close all
 
+% http://www.thphys.uni-heidelberg.de/~gasenzer/index.php?n1=teaching&n2=chaos
 
-l = 1;   % m
-g = 1.35; % m/s^2
-q = 2;
-omega_d = 0.6667; % rad/s
-F_d = 1;
+% damped oscillator
 
-param = [l, g, q, omega_d, F_d];
+%g = 0;q = 2;omega_d = 0.6667;
+% transient, then periodic
+%g = 1.35; q=2; omega_d = 0.6667;
+% chaotic
+g = 1.15; q=4; omega_d = 0.6667;
 
-[t, X] = ode45(@damped_driven_ode, [0, 2000], [-pi/3, 0], [], param);
+param = [q, g, omega_d];
 
-plot(X(:,1), X(:,2))
-xlabel('position');
-ylabel('velocity');
+[t, X] = ode45(@damped_driven_ode, [0, 1000], [0, -2*pi/3, 0], [], param);
+
+% Wrap theta between -pi, pi
+X(:,2) = wrapToPi(X(:,2));
+
+figure
+plot(X(:,2), X(:,1))
+ylabel('omega (ang. velocity)');
+xlabel('theta (angle)');
+title('Chaotic behaviour in damped driven pendulum');
+
+% Now take poincare diagram by selecting only points corresponding to
+% our angular frequency omega_d
+
+% We need a LOT of points for this. solve the ODE in sections and
+% throw away extraneous points each time
+
+figure
+hold on
+ylabel('omega (ang. velocity)');
+xlabel('theta (angle)');
+title('Poincare section in phase with driving force');
+omega = 0;
+theta = -2*pi/3;
+phi = 0;
+for i=1:100
+    g = 1.15; q=4; omega_d = 0.6667;
+
+    param = [q, g, omega_d];
+    
+    [t, X] = ode45(@damped_driven_ode, [0, 10000*(1/omega_d)], [omega, theta, phi], [], param);
+
+    % Wrap theta between -pi, pi
+    X(:,2) = wrapToPi(X(:,2));
+    
+    % plot in-phase points
+    poincare = find(sin(X(:,3)) > 0.99999)
+
+    plot([X(poincare,2)], [X(poincare,1)], '.')
+    omega = X(end, 1);
+    theta = X(end, 2);
+    phi = X(end, 3);
+    pause(0.01);
+end
